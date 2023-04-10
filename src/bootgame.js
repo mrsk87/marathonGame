88623aa707197358d64667d9e5ba1e364191c751
mrsk87 <m4r10i@gmail.com>
2023-04-10 19:46:47 +0100

    boot_wingame_2scenes

--- src/bootgame.js
+++ src/bootgame.js
@@ -4,23 +4,56 @@ class bootgame extends Phaser.Scene {
     super('bootgame');
   }
 
+  preload(){
+    this.load.image('skyBoot', 'assets/skyBoot.png');
+    this.load.image('startbtn', 'assets/start.png');
+    //this.load.audio('win', 'assets/wingame.mp3');
+}
+
   create() {
+
+    this.add.image(400, 300, 'skyBoot');
+       //this.scene.start("Level2")
+
+      //  this.win = this.sound.add('win');
+      //     this.win.play();
 
      //this.scene.start("Level2")
 
-    this.add.text(16, 16, 'Marathon Game', { fontSize: '32px', fill: '#000' });
-    console.log("bootgame");
-    //this.scene.start("Level1");
+    // this.add.text(306, 306, 'Marathon Game', { fontSize: '32px', fill: '#000' });
+    // console.log("bootgame");
+    // //this.scene.start("Level1");
 
     
     
+    this.startButon();
 
-    const clickButton = this.add.text(100, 100, 'Click me!', { fill: '#0f0' })
-      .setInteractive()
-      .on('pointerdown', () => this.scene.start("Level1", {score: 0}) );
+
+    // const clickButton = this.add.text(300, 300, 'Start Game!', { fill: '#000' })
+    //   .setInteractive()
+    //   .on('pointerdown', () => this.scene.start("Level1", {score: 0}) );
 
     
   }
+
+  startButon(){
+    //Botao restart
+    const startBtn = this.add.image(400, 300, 'startbtn')
+    .setScale(1)
+    startBtn.setInteractive();
+
+    startBtn.on('pointerdown', () => {
+        
+        this.scene.start("Level1", {score: 0});
+        
+        
+    });
+
+    
+
+    
+
+}
 
 }
 
--- src/wingame.js
+++ src/wingame.js
@@ -7,6 +7,7 @@ class wingame extends Phaser.Scene {
     preload(){
         this.load.image('skywin', 'assets/skywingame.png');
         this.load.audio('win', 'assets/wingame.mp3');
+        this.load.image('restartBtn', 'assets/restart.png');
     }
   
     create() {
@@ -16,18 +17,48 @@ class wingame extends Phaser.Scene {
        this.win = this.sound.add('win');
           this.win.play();
   
-      this.add.text(306, 306, 'WINNER!!!', { fontSize: '32px', fill: '#000' });
+      this.add.text(306, 306, 'WINNER!!!', { fontSize: '52px', fill: '#000' });
       console.log("wingame");
       //this.scene.start("Level1");
   
       
+      this.botaoRestart();
+  
       
   
-      const clickButton = this.add.text(100, 100, 'Restart Game!', { fill: '#0f0' })
+      
+    }
+
+    botaoRestart(){
+        //Botao restart
+        const restartBtn = this.add.image(230, 400, 'restartBtn')
+        .setScale(0.1)
+        restartBtn.setInteractive();
+
+        restartBtn.on('pointerdown', () => {
+            restartBtn.setTint(0xff00ff);
+            this.scene.start("Level1", {score: 0});
+            
+            
+        });
+
+        restartBtn.on('pointerup', () => {
+            restartBtn.clearTint();
+        });
+
+        restartBtn.on('pointerover', () => {
+            restartBtn.setScale(0.11);
+        });
+
+        restartBtn.on('pointerout', () => {
+            restartBtn.setScale(0.1);
+        });
+
+        //Botao para fazer restart game quando ha gameover
+        const clickrestart = this.add.text(300, 400, 'Restart Game!', { fontSize: '32px',fill: 'red' })
         .setInteractive()
-        .on('pointerdown', () => this.scene.start("Level1", {score: 0}) );
-  
-      
+        .on('pointerdown', () => this.scene.start("Level1", {score: 0}));
+
     }
   
   }
